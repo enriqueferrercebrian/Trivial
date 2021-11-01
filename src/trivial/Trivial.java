db38719cc71e55809123eaf88c5e649599c7e0f4
Enrique Ferrer Cebrian <enriqueferrercebrian@gmail.com>
2021-11-02 00:02:37 +0100

    Ranking y bonito

--- src/trivial/Trivial.java
+++ src/trivial/Trivial.java
@@ -62,7 +62,7 @@ public class Trivial {
         double tmpAciertos;
         String tmpUsuario;
         double porcentajeDeAciertos;
-        System.out.println("Bienvenidos al trivial de " + NOMBRE_TRIVIAL + ".");
+        System.out.println("Bienvenidos al trivial de " + NOMBRE_TRIVIAL + ". \n----------------------------------------------------\n");
 
         while (jugar != false) {
             contadorPreguntas = 0;
@@ -78,22 +78,22 @@ public class Trivial {
             aciertos3 = 0;
             aciertos4 = 0;
             while (numeroJugadores < 1 || numeroJugadores > 4) {
-                System.out.println("Cuantos jugadores vais a jugar? maximo 4 minimo 1");
+                System.out.println("Cuantos jugadores vais a jugar? maximo 4 minimo 1 \n----------------------------------------------------\n");
                 try {
                     numeroJugadores = Integer.parseInt(sc.nextLine());
                     if (numeroJugadores < 1 || numeroJugadores > 4) {
-                        System.out.println("Porfavor, solo de 1 a 4 jugadores.");
+                        System.out.println("Porfavor, solo de 1 a 4 jugadores. \n----------------------------------------------------\n");
 
                     }
                 } catch (NumberFormatException ex) {
-                    System.out.println("Porfavor, inserte numeros no letras.");
+                    System.out.println("Porfavor, inserte numeros no letras. \n----------------------------------------------------\n");
                 } catch (Exception ex) {
                     System.out.println(ERRORPARSEINTEXCEPTION);
                 }
             }
             while (contadorJugadores < numeroJugadores) {
                 if ((contadorJugadores + 1) == 1) {
-                    System.out.println("Cual es tu nombre jugador " + (contadorJugadores + 1));
+                    System.out.println("Cual es tu nombre jugador " + (contadorJugadores + 1) + "\n----------------------------------------------------\n");
                     contadorJugadores++;
                     usuario1 = sc.nextLine();
 
@@ -101,12 +101,12 @@ public class Trivial {
                     contadorPreguntas = 0;
                     aciertos = 0;
                     repetida1 = repetida2 = repetida3 = repetida4 = repetida5 = repetida6 = repetida7 = repetida8 = repetida9 = repetida10 = false;
-                    System.out.println("Cual es tu nombre jugador " + (contadorJugadores + 1));
+                    System.out.println("Cual es tu nombre jugador " + (contadorJugadores + 1) + "\n----------------------------------------------------\n");
                     contadorJugadores++;
                     usuario2 = sc.nextLine();
 
                 } else if ((contadorJugadores + 1) == 3) {
-                    System.out.println("Cual es tu nombre jugador " + (contadorJugadores + 1));
+                    System.out.println("Cual es tu nombre jugador " + (contadorJugadores + 1) + "\n----------------------------------------------------\n");
                     contadorPreguntas = 0;
                     aciertos = 0;
                     repetida1 = repetida2 = repetida3 = repetida4 = repetida5 = repetida6 = repetida7 = repetida8 = repetida9 = repetida10 = false;
@@ -114,7 +114,7 @@ public class Trivial {
                     usuario3 = sc.nextLine();
 
                 } else if ((contadorJugadores + 1) == 4) {
-                    System.out.println("Cual es tu nombre jugador " + (contadorJugadores + 1));
+                    System.out.println("Cual es tu nombre jugador " + (contadorJugadores + 1) + "\n----------------------------------------------------\n");
                     contadorPreguntas = 0;
                     aciertos = 0;
                     repetida1 = repetida2 = repetida3 = repetida4 = repetida5 = repetida6 = repetida7 = repetida8 = repetida9 = repetida10 = false;
@@ -123,21 +123,21 @@ public class Trivial {
                 }
 
                 while (numPreguntas < 3 || numPreguntas > 10) {
-                    System.out.println("¿Cuantas preguntas quieres hacer? De 3 a 10 preguntas.");
+                    System.out.println("¿Cuantas preguntas quieres hacer? De 3 a 10 preguntas. \n----------------------------------------------------\n");
                     numeroPreguntasText = sc.nextLine();
 
                     try {
                         numPreguntas = Integer.parseInt(numeroPreguntasText);
                         if (numPreguntas < 3 || numPreguntas > 10) {
-                            System.out.println("Porfavor, solo numeros del 3 al 10");
+                            System.out.println("Porfavor, solo numeros del 3 al 10 \n----------------------------------------------------\n");
                         }
                     } catch (NumberFormatException ex) {
-                        System.out.println("Porfavor, inserte numeros no letras.");
+                        System.out.println("Porfavor, inserte numeros no letras. \n----------------------------------------------------\n");
                     } catch (Exception ex) {
                         System.out.println(ERRORPARSEINTEXCEPTION);
                     }
                 }
-                System.out.println("Responde a las preguntas con S (si) o N (No)");
+                System.out.println("Responde a las preguntas con S (si) o N (No) \n----------------------------------------------------\n");
 
                 while (contadorPreguntas < numPreguntas) {
                     numRamdon = (int) Math.floor(Math.random() * (RANGO_MAXIMO - RANGO_MINIMO + 1) + RANGO_MINIMO);
@@ -173,7 +173,7 @@ public class Trivial {
 
                             }
                         } else {
-                            System.out.println("\nSon 5: Homer, Marge, Bart, Maggie y Lisa");
+                            System.out.println("\nSon 5: Homer, Marge, Bart, Maggie y Lisa \n----------------------------------------------------\n");
                         }
                     } else if (numRamdon == 2 && !repetida2) {
                         repetida2 = true;
@@ -204,7 +204,7 @@ public class Trivial {
 
                             }
                         } else {
-                            System.out.println("\nRealmente fue ella quien disparó, asi que si.");
+                            System.out.println("\nRealmente fue ella quien disparó, asi que si. \n----------------------------------------------------\n");
                         }
                     } else if (numRamdon == 3 && !repetida3) {
                         repetida3 = true;
@@ -234,7 +234,7 @@ public class Trivial {
 
                             }
                         } else {
-                            System.out.println("\nLa ciudad rival de Springfield es shelbyville");
+                            System.out.println("\nLa ciudad rival de Springfield es shelbyville \n----------------------------------------------------\n");
 
                         }
                     } else if (numRamdon == 4 && !repetida4) {
@@ -266,7 +266,7 @@ public class Trivial {
 
                             }
                         } else {
-                            System.out.println("\nNo son pareja, aunque siempre aparecen juntos, se trata de dos buenos amigos.");
+                            System.out.println("\nNo son pareja, aunque siempre aparecen juntos, se trata de dos buenos amigos.\n----------------------------------------------------\n");
                         }
                     } else if (numRamdon == 5 && !repetida5) {
                         repetida5 = true;
@@ -296,7 +296,7 @@ public class Trivial {
 
                             }
                         } else {
-                            System.out.println("\nLos simpsons viajan a Japon con unos billetes que le roban a Ned Flanders.");
+                            System.out.println("\nLos simpsons viajan a Japon con unos billetes que le roban a Ned Flanders.\n----------------------------------------------------\n");
                         }
                     } else if (numRamdon == 6 && !repetida6) {
                         repetida6 = true;
@@ -326,7 +326,7 @@ public class Trivial {
 
                             }
                         } else {
-                            System.out.println("\nTiene un hermano, el cual le pidio dinero para hacer un traductor de bebes, lo invento y triunfó.");
+                            System.out.println("\nTiene un hermano, el cual le pidio dinero para hacer un traductor de bebes, lo invento y triunfó. \n----------------------------------------------------\n");
 
                         }
                     } else if (numRamdon == 7 && !repetida7) {
@@ -357,7 +357,7 @@ public class Trivial {
 
                             }
                         } else {
-                            System.out.println("\nBart tiene un hermano gemelo llamado Hugo, el cual aparecio en un episodio de hallowen.");
+                            System.out.println("\nBart tiene un hermano gemelo llamado Hugo, el cual aparecio en un episodio de hallowen. \n----------------------------------------------------\n");
                         }
                     } else if (numRamdon == 8 && !repetida8) {
                         repetida8 = true;
@@ -387,8 +387,8 @@ public class Trivial {
 
                             }
                         } else {
-                            System.out.println("\nSegun un Brujo adivino, jefe de un casino, que predie el futuro ");
-                            System.out.println("en los Simpsons, Lisa acabaria de Presidenta de los EE.UU");
+                            System.out.println("\nSegun un Brujo adivino, jefe de un casino, que predie el futuro\n ");
+                            System.out.println("en los Simpsons, Lisa acabaria de Presidenta de los EE.UU \n----------------------------------------------------\n");
                         }
                     } else if (numRamdon == 9 && !repetida9) {
                         repetida9 = true;
@@ -418,7 +418,7 @@ public class Trivial {
 
                             }
                         } else {
-                            System.out.println("\nRealmente si, Dios y Jesucristo, aparecen resprentados con 5 dedos.");
+                            System.out.println("\nRealmente si, Dios y Jesucristo, aparecen resprentados con 5 dedos. \n----------------------------------------------------\n");
 
                         }
                     } else if (numRamdon == 10 && !repetida10) {
@@ -441,7 +441,7 @@ public class Trivial {
                                 if (RESPUESTA10.charAt(0) == respuestaUsuario.charAt(0)
                                         || RESPUESTA10.charAt(1) == respuestaUsuario.charAt(1)) {
                                     repetida10 = true;
-                                    System.out.println("\nCasi, casi, venga te doy medio punto.");
+                                    System.out.println("\nCasi, casi, venga te doy medio punto. \n----------------------------------------------------\n");
                                     aciertos = aciertos + 0.5;
                                     if (aciertos > 1) {
                                         System.out.println("\nLlevas un total de " + aciertos + " Puntos, Animo!. \n----------------------------------------------------");
@@ -450,16 +450,16 @@ public class Trivial {
 
                                     }
                                 } else {
-                                    System.out.println("\nEl pelo y la oreja de homer forman las iniciales del creador MG.");
+                                    System.out.println("\nEl pelo y la oreja de homer forman las iniciales del creador MG. \n----------------------------------------------------\n");
 
                                 }
                             } else {
-                                System.out.println("\nlas palabras no tienen la misma longitud, creo que no te la sabes...");
+                                System.out.println("\nlas palabras no tienen la misma longitud, creo que no te la sabes... \n----------------------------------------------------\n");
                             }
                         }
                     }
 
-                    switch ((contadorJugadores)) {
+                    switch (contadorJugadores) {
                         case 1:
                             aciertos1 = aciertos;
                         case 2:
@@ -470,82 +470,87 @@ public class Trivial {
                             aciertos4 = aciertos;
 
                     }
-                    System.out.println(usuario1 + usuario2 + usuario3 + usuario4);
-
-                   
 
                 }
                 porcentajeDeAciertos = (aciertos * 100) / numPreguntas;
                 if (porcentajeDeAciertos <= 33) {
-                    System.out.print("\n----------------------------------------------------\nSubscribete a Disney plus y empiezala de nuevo.\n----------------------------------------------------\n");
+                    System.out.print("Subscribete a Disney plus y empiezala de nuevo.\n----------------------------------------------------\n");
                 } else if (porcentajeDeAciertos <= 66) {
-                    System.out.print("\n----------------------------------------------------\nNo prestaste mucha atención a la serie.\n----------------------------------------------------\n");
+                    System.out.print("No prestaste mucha atención a la serie.\n----------------------------------------------------\n");
                 } else if (porcentajeDeAciertos <= 99.9) {
-                    System.out.print("\n----------------------------------------------------\nVeo que te gusta la serie, enhorabuena.\n----------------------------------------------------\n");
+                    System.out.print("Veo que te gusta la serie, enhorabuena.\n----------------------------------------------------\n");
                 } else if (porcentajeDeAciertos <= 100) {
-                    System.out.print("\n----------------------------------------------------\n¿Eres Matt Groening? PER-FEC-TO!\n----------------------------------------------------\n");
+                    System.out.print("¿Eres Matt Groening? PER-FEC-TO!\n----------------------------------------------------\n");
                 }
 
-                System.out.println(String.format("Has acertado el  %.02f", porcentajeDeAciertos) + "% de las preguntas.");
+                System.out.println(String.format("Has acertado el  %.02f", porcentajeDeAciertos) + "% de las preguntas.\n----------------------------------------------------");
 
             }
-             if (aciertos1 < aciertos2) {
-                        tmpAciertos = aciertos1; //tmpAciertos = 6
-                        tmpUsuario = usuario1;
-                        aciertos1 = aciertos2; // a = 8
-                        usuario1 = usuario2;
-                        aciertos2 = tmpAciertos; // b = 6
-                        usuario2 = tmpUsuario;
+            if (aciertos1 < aciertos2) {
+                tmpAciertos = aciertos1; //tmpAciertos = 6
+                tmpUsuario = usuario1;
+                aciertos1 = aciertos2; // a = 8
+                usuario1 = usuario2;
+                aciertos2 = tmpAciertos; // b = 6
+                usuario2 = tmpUsuario;
 
-                        // a= 8    b=6
-                    }
+                // a= 8    b=6
+            }
 
-                    if (aciertos3 < aciertos4) {
-                        tmpAciertos = aciertos3;
-                        tmpUsuario = usuario3;
-                        aciertos3 = aciertos4;
-                        usuario3 = usuario4;
-                        aciertos4 = tmpAciertos;
-                        usuario4 = tmpUsuario;
-                    }
+            if (aciertos3 < aciertos4) {
+                tmpAciertos = aciertos3;
+                tmpUsuario = usuario3;
+                aciertos3 = aciertos4;
+                usuario3 = usuario4;
+                aciertos4 = tmpAciertos;
+                usuario4 = tmpUsuario;
+            }
 
-                    if (aciertos1 < aciertos3) {
+            if (aciertos1 < aciertos3) {
 
-                        tmpAciertos = aciertos1;
-                        tmpUsuario = usuario1;
+                tmpAciertos = aciertos1;
+                tmpUsuario = usuario1;
 
-                        aciertos1 = aciertos3;
-                        usuario1 = usuario3;
+                aciertos1 = aciertos3;
+                usuario1 = usuario3;
 
-                        aciertos3 = tmpAciertos;
-                        usuario3 = tmpUsuario;
+                aciertos3 = tmpAciertos;
+                usuario3 = tmpUsuario;
 
-                    }
+            }
 
-                    if (aciertos2 < aciertos4) {
-                        tmpAciertos = aciertos2;
-                        tmpUsuario = usuario2;
+            if (aciertos2 < aciertos4) {
+                tmpAciertos = aciertos2;
+                tmpUsuario = usuario2;
 
-                        aciertos2 = aciertos4;
-                        usuario2 = usuario4;
+                aciertos2 = aciertos4;
+                usuario2 = usuario4;
 
-                        aciertos4 = tmpAciertos;
-                        usuario4 = tmpUsuario;
+                aciertos4 = tmpAciertos;
+                usuario4 = tmpUsuario;
 
-                    }
-                    if (aciertos2 < aciertos3) {
+            }
+            if (aciertos2 < aciertos3) {
 
-                        tmpAciertos = aciertos2;
-                        tmpUsuario = usuario2;
+                tmpAciertos = aciertos2;
+                tmpUsuario = usuario2;
 
-                        aciertos2 = aciertos3;
-                        usuario2 = usuario3;
+                aciertos2 = aciertos3;
+                usuario2 = usuario3;
 
-                        aciertos3 = tmpAciertos;
-                        usuario3 = tmpUsuario;
-                    }
+                aciertos3 = tmpAciertos;
+                usuario3 = tmpUsuario;
+            }
 
-            System.out.println("Este es el ranking: " + "\n" + " " + aciertos1 + " " + usuario1 + " \n " + aciertos2 + " " + usuario2 + " \n " + aciertos3 + " " + usuario3 + " \n " + aciertos4 + " " + usuario4);
+            if (numeroJugadores == 1) {
+                System.out.println("");
+            } else if (numeroJugadores == 2) {
+                System.out.println("Este es el ranking: " + "\n" + " " + aciertos1 + " " + usuario1 + " \n " + aciertos2 + " " + usuario2);
+            } else if (numeroJugadores == 3) {
+                System.out.println("Este es el ranking: " + "\n" + " " + aciertos1 + " " + usuario1 + " \n " + aciertos2 + " " + usuario2 + " \n " + aciertos3 + " " + usuario3);
+            } else if (numeroJugadores == 4) {
+                System.out.println("Este es el ranking: " + "\n" + " " + aciertos1 + " " + usuario1 + " \n " + aciertos2 + " " + usuario2 + " \n " + aciertos3 + " " + usuario3 + " \n " + aciertos4 + " " + usuario4);
+            }
 
             System.out.println("\n¿Quieres volver a jugar?");
             volverAJugar = sc.nextLine();
